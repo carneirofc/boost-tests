5165f9921d2c33a96cf928b86b8ce1fe01b1e6ef
carneirofc <claudiofcarneiro@hotmail.com>
2020-03-31 10:34:58 -0300

    regatron: Include a virtual destructor

--- regatron-interface/src/net/Handler.hpp
+++ regatron-interface/src/net/Handler.hpp
@@ -7,5 +7,6 @@ namespace Net{
     class Handler{
         public:
             virtual const std::string handle(const std::string& message);
+            virtual ~Handler(){}
     };
 } // namespace nameNe
